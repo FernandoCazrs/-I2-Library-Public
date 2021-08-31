990864cefd76fe1701ec780d36f4bd7e05509f53
Marco RBB <89417435+BolaDeGrasa@users.noreply.github.com>
2021-08-31 13:49:02 -0500

    Update areaTriangulo.c

--- lib/src/areaTriangulo.c
+++ lib/src/areaTriangulo.c
@@ -4,7 +4,7 @@
  * @param b El valor de un lado del triángulo
  * @param h El valor de la altura del triángulo
  * @returns El resultado del cálculo
- * */
+ */
 float areaTriangulo(float b, float h){
     float r;
     r= (b*h)/2;
