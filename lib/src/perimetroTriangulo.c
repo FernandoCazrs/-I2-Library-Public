c680067cfe58779e77bbcd18a87adf2b17adcc65
Marco RBB <89417435+BolaDeGrasa@users.noreply.github.com>
2021-08-31 13:49:37 -0500

    Update perimetroTriangulo.c

--- lib/src/perimetroTriangulo.c
+++ lib/src/perimetroTriangulo.c
@@ -3,7 +3,7 @@
 /**@brief Calcula el perímetro de un triángulo
  * @param x El valor del lado del triángulo
  * @returns El resultado del cálculo
- * */
+*/
 float perimetroTriangulo(float x){
     return x+x+x;
 }
