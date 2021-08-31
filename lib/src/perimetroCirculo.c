d345dc50642faa57486a48aeff6494424ef93a25
Marco RBB <89417435+BolaDeGrasa@users.noreply.github.com>
2021-08-31 13:49:21 -0500

    Update perimetroCirculo.c

--- lib/src/perimetroCirculo.c
+++ lib/src/perimetroCirculo.c
@@ -3,7 +3,7 @@
 /**@brief Calcula el perímetro de un círculo
  * @param x El valor del radio de un círculo
  * @returns El resultado del cálculo
- * */
+ */
 float perimetroCirculo(float x){
     float r;
     r= 2*M_PI*x;
