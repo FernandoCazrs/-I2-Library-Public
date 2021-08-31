f0e0e524e174085955c389be1ae6dc4f67f650d4
Marco RBB <89417435+BolaDeGrasa@users.noreply.github.com>
2021-08-31 13:50:32 -0500

    Update volumenPrismaPentagonal.c

--- lib/src/volumenPrismaPentagonal.c
+++ lib/src/volumenPrismaPentagonal.c
@@ -4,7 +4,7 @@
  * @param a El valor de un lado de la base
  * @param H El valor de la altura del prisma pentagonal
  * @returns El resultado del cálculo
- * */
+ */
 float volumenPrismaPentagonal(float a, float H){
     float r, area;
     area=( (0.25)* sqrtf(5 * (5+(2*sqrtf(5) ) ) )* (a*a));
